bd9ea508f0d4c970738ad50e8f1ed9d9f917ae07
Ckst123 <cdef3456@knu.ac.kr>
2021-11-06 12:06:51 +0900

    add focal loss

--- models.py
+++ models.py
@@ -26,6 +26,7 @@ from typing import Optional, Tuple
 import torch
 import torch.utils.checkpoint
 from torch import nn
+import torch.nn.functional as F
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss, MultiLabelSoftMarginLoss
 
 from transformers import BertPreTrainedModel, BertModel
@@ -168,6 +169,161 @@ class BertForMultipleLabelSequenceClassification(BertPreTrainedModel):
             loss = loss_fct(logits, labels)
         
         logits = torch.stack([torch.zeros_like(logits), logits] ,dim=-1)
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class FocalCrossEntropyLoss(nn.Module):
+
+    def __init__(self, gamma=2.):
+        super().__init__()
+        self.gamma = gamma
+
+    def forward(self, logits, targets):
+        prob = F.softmax(logits, dim=-1)
+        targets_one_hot = torch.zeros_like(prob) 
+        targets_one_hot.scatter_(1, targets.unsqueeze(1), 1)
+        w = torch.pow(1. - prob, self.gamma)
+        focal = - w * torch.log(prob + 1e-8)
+        focal = focal.sum(dim=-1)
+        return focal
+
+
+class BertForMultipleSequenceClassificationWithFocalLoss(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.bert = BertModel(config)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+
+        self.classifiers = nn.ModuleList([
+            nn.Linear(config.hidden_size, num)
+            for num in config.num_classes
+        ])
+
+        self.init_weights()
+
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = tuple([classifier(pooled_output) for classifier in self.classifiers])
+
+        loss = None
+        if labels is not None:
+            loss = []
+
+            for logit, label, num in zip(logits, labels.transpose(1, 0), self.config.num_classes):
+                loss_fct = FocalCrossEntropyLoss()
+                loss.append(loss_fct(logit.view(-1, num), label.view(-1)))
+            loss = torch.mean(torch.stack(loss))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+class BertForMultipleLabelSequenceClassificationWithFocalLoss(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.bert = BertModel(config)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+
+        self.classifier = nn.Linear(config.hidden_size, self.num_labels)
+
+        self.init_weights()
+
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        logits = torch.stack([torch.zeros_like(logits), logits] ,dim=-1)
+
+        if labels is not None:
+            loss_fct = FocalCrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, 2), labels.view(-1)).mean()
 
         if not return_dict:
             output = (logits,) + outputs[2:]
